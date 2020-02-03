ae42da89e79ceaa174ed798af2a189ff5a372ace
jaycyn1994 <60630855+jaycyn1994@users.noreply.github.com>
2020-02-03 17:15:57 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,10 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-let package = Package(
-    name: "ImageRow",
-    platforms: [
-        .iOS("9.3")
-    ],
-    products: [
-        .library(name: "ImageRow", targets: ["ImageRow"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/AndrewBennet/Eureka", from: "5.0.0")
-    ],
-    targets: [
-        .target(name: "ImageRow", dependencies: ["Eureka"], path: "Sources"),
-        .testTarget(name: "ImageRowTests", dependencies: ["ImageRow"], path: "Tests")
-    ],
-    swiftLanguageVersions: [.v5]
-)
+let package = Package(name: "ImageRow",
+                      platforms: [.iOS(.v10)],
+                      products: [.library(name: "ImageRow",
+                                          targets: ["ImageRow"])],
+                      targets: [.target(name: "ImageRow",
+                                        path: "Source")],
+                      swiftLanguageVersions: [.v5])
